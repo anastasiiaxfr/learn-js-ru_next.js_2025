60df84c30c075e1e62fe036df6217c84ce81614f
anastasiiaxfr <anastasiiaberest@gmail.com>
2025-06-11 14:49:19 +0300

    hw-2

---
+++ src/components/layout/NavMenu.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+const menu = [
+  { url: "/", title: "Home" },
+  { url: "/rackets", title: "Rackets" },
+];
+
+function NavMenu() {
+  const pathname = usePathname();
+  return (
+    <NavigationMenu className="menu">
+      <NavigationMenuList>
+        {menu.map((i, ind) => (
+          <NavigationMenuItem key={ind}>
+            <Link href={i.url} className={pathname === i.url ? "active" : ""}>
+              {i.title}
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+export default NavMenu;
